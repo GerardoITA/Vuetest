7e66716fbf73400062959b0f4558f62866548c9d
GerardoITA <gerardo.giusti@tiscali.it>
2024-07-11 00:32:54 +0200

    creazione template e inizio struttura design

---
+++ vuetest/src/components/Template.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: "",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+   
+    },
+    mounted() {
+    
+    },
+    computed: {
+
+    },
+}
+</script>
+
+<template>
+    <h2 class="c-accent-dark">
+        test
+    </h2>
+</template>
+
